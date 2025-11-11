2e8093639f22d8d3c6f8fdb5d75326b3967648e5
justarunsthing <justarunsthing@outlook.com>
2025-11-11 08:31:13 +0000

    Declare nullables & initialize collections

--- Models/MovieListResponse.cs
+++ Models/MovieListResponse.cs
@@ -3,7 +3,7 @@
     public class MovieListResponse
     {
         public int Page { get; set; }
-        public List<Movie> Results { get; set; }
+        public List<Movie> Results { get; set; } = [];
         public int TotalPages { get; set; }
         public int TotalResults { get; set; }
     }
