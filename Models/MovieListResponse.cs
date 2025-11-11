1c965680196c5b14f742b8f512ec82e8443ae7fb
justarunsthing <justarunsthing@outlook.com>
2025-11-11 08:29:05 +0000

    Added MovieListResponse class

---
+++ Models/MovieListResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace MovieProBlazor.Models
+{
+    public class MovieListResponse
+    {
+        public int Page { get; set; }
+        public List<Movie> Results { get; set; }
+        public int TotalPages { get; set; }
+        public int TotalResults { get; set; }
+    }
+}
