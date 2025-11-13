9349ecdd4fd3db032bc8ba47af26e6968b34900b
justarunsthing <justarunsthing@outlook.com>
2025-11-13 08:06:50 +0000

    Added TmdbService

---
+++ Services/TmdbService.cs
@@ -0,0 +1,19 @@
+﻿namespace MovieProBlazor.Services
+{
+    public class TmdbService
+    {
+        private readonly HttpClient _http;
+        private readonly string? _tmdbAccessKey;
+
+        public TmdbService(HttpClient httpClient, IConfiguration config)
+        {
+            _http = httpClient;
+            _tmdbAccessKey = config["TmdbAccessKey"];
+
+            if (!string.IsNullOrEmpty(_tmdbAccessKey))
+            {
+                _http.DefaultRequestHeaders.Authorization = new("Bearer", _tmdbAccessKey);
+            }
+        }
+    }
+}
