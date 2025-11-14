f4d22e60f5aa537ac61400bb1e45fe29b6c00261
justarunsthing <justarunsthing@outlook.com>
2025-11-14 08:39:50 +0000

    Implemented GetFavourites method

--- Services/FavouritesService.cs
+++ Services/FavouritesService.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.JSInterop;
+﻿using System.Text.Json;
+using Microsoft.JSInterop;
+using MovieProBlazor.Models;
 
 namespace MovieProBlazor.Services
 {
@@ -10,6 +12,23 @@ namespace MovieProBlazor.Services
         public FavouritesService(IJSRuntime jSRuntime)
         {
             _jsRuntime = jSRuntime;
+        }
+
+        public async Task<List<Movie>> GetFavourites()
+        {
+            List<Movie> movies = [];
+
+            try
+            {
+                var json = await _jsRuntime.InvokeAsync<string>("localStorage.getItem", _localStorageKey);
+                movies = JsonSerializer.Deserialize<List<Movie>>(json) ?? [];
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            return movies;
         }
     }
 }
