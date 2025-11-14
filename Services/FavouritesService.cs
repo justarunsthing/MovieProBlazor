60a7a3832e43f85f67f19f5b0721615713266018
justarunsthing <justarunsthing@outlook.com>
2025-11-14 08:39:20 +0000

    Added skeleton FavouritesService

---
+++ Services/FavouritesService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.JSInterop;
+
+namespace MovieProBlazor.Services
+{
+    public class FavouritesService
+    {
+        private readonly string _localStorageKey = "favourites";
+        private readonly IJSRuntime _jsRuntime;
+
+        public FavouritesService(IJSRuntime jSRuntime)
+        {
+            _jsRuntime = jSRuntime;
+        }
+    }
+}
