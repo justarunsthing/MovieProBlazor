dd71b82369202e018bcadcb47bfde37ea1235477
justarunsthing <justarunsthing@outlook.com>
2025-11-18 07:59:50 +0000

    Added classes for movie videos

---
+++ Models/MovieVideosResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace MovieProBlazor.Models
+{
+    public class MovieVideosResponse
+    {
+        public int Id { get; set; }
+        public List<Video> Results { get; set; } = [];
+    }
+}
