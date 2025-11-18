597c9feec51b18373e05e5f342f1019709e52b02
justarunsthing <justarunsthing@outlook.com>
2025-11-18 09:06:03 +0000

    Added JS file for movie by id component

---
+++ Components/Pages/MovieById.razor.js
@@ -0,0 +1,23 @@
+﻿let src = "";
+let modal = null;
+
+function createEmbed() {
+    document.getElementById("movie-trailer").setAttribute("src", src);
+}
+
+function destroyEmbed() {
+    document.getElementById("movie-trailer").setAttribute("src", "");
+}
+
+export function initVideoPlayer(videoUrl) {
+    if (modal) {
+        modal.removeEventListener("shown.bs.modal", createEmbed);
+        modal.removeEventListener("hidden.bs.modal", destroyEmbed);
+    }
+
+    modal = document.getElementById("trailer-modal");
+    src = videoUrl;
+
+    modal.addEventListener("shown.bs.modal", createEmbed);
+    modal.addEventListener("hidden.bs.modal", destroyEmbed);
+}
