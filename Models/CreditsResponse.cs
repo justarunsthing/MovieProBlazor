31c4ea0723acc5d1c804ee7e7f968f195a3f1317
justarunsthing <justarunsthing@outlook.com>
2025-11-18 18:47:09 +0000

    Added models for movie cast & crew

---
+++ Models/CreditsResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace MovieProBlazor.Models
+{
+    public class CreditsResponse
+    {
+        public int Id { get; set; }
+        public List<Cast> Casts { get; set; } = [];
+        public List<Crew> Crew { get; set; } = [];
+    }
+}
