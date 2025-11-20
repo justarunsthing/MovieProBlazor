7d2db028b2de9b8007ac11f07c9710a564b1f38d
justarunsthing <justarunsthing@outlook.com>
2025-11-20 17:51:42 +1000

    Added Json property name attribute

--- Models/CreditsResponse.cs
+++ Models/CreditsResponse.cs
@@ -1,9 +1,15 @@
-﻿namespace MovieProBlazor.Models
+﻿using System.Text.Json.Serialization;
+
+namespace MovieProBlazor.Models
 {
     public class CreditsResponse
     {
         public int Id { get; set; }
+
+        [JsonPropertyName("cast")]
         public List<Cast> Casts { get; set; } = [];
+
+        [JsonPropertyName("crew")]
         public List<Crew> Crew { get; set; } = [];
     }
 }
