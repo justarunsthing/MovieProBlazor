194acf2f7ca7bd5b6b0f36dec7632cddcb70cffa
justarunsthing <justarunsthing@outlook.com>
2025-11-23 20:11:12 +1000

    Added edge function for Netlify

---
+++ Netlify/EdgeFunctions/tmdb.js
@@ -0,0 +1,21 @@
+﻿export default async (request, context) => {
+    const key = Netlify.env.get('API_KEY');
+    let tmdbUrl = Netlify.env.get('API_URL');
+
+    if (!tmdbUrl.endsWith('/')) {
+        tmdbUrl += '/';
+    }
+
+    const url = new URL(request.url);
+    const tmdbPath = url.pathname.replace('/tmdb/', ''); // /tmdb/movies/popular => movies/popular
+
+    return await fetch(tmdbUrl + tmdbPath + url.search, {
+        headers: {
+            'Authorization': `Bearer ${key}`
+        }
+    });
+}
+
+export const config = {
+    path: '/tmdb/*'
+}
